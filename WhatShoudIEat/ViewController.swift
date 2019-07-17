309b2b32e0a80933e9c1a799c447b5536e62ce45
Klaudia Popow <klaudia.popow@grandparade.co.uk>
2019-07-17 11:43:38 +0200

    first commit

---
+++ WhatShoudIEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatShoudIEat
+//
+//  Created by Klaudia Popow on 17/07/2019.
+//  Copyright © 2019 Klaudia Popow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
